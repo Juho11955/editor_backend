e0f87f43f897c8afda136ff56739d741fce825c6
juho <orot1195@gmail.com>
2023-11-07 13:25:45 +0900

    DTO modify

--- src/main/java/com/post/dev/controller/ApproveController.java
+++ src/main/java/com/post/dev/controller/ApproveController.java
@@ -1,10 +1,21 @@
 package com.post.dev.controller;
 
+import lombok.val;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
 
 @Controller
 public class ApproveController {
+
+    @GetMapping(value = "approveList", produces = "application/json; charset=utf8")
+    @ResponseBody
+    public ()
+
 
 
 
