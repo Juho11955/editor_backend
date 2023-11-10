24ffc7058d9cd3d31e1694fb7a7b0800be6be8aa
juho <orot1195@gmail.com>
2023-11-10 16:21:08 +0900

    controller, service modify

--- src/main/java/com/post/dev/controller/ApproveController.java
+++ src/main/java/com/post/dev/controller/ApproveController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
+import java.util.List;
 
 @Controller
 public class ApproveController {
--- src/main/java/com/post/dev/service/approve/impl/ApproveServiceImpl.java
+++ src/main/java/com/post/dev/service/approve/impl/ApproveServiceImpl.java
@@ -1,12 +1,27 @@
 package com.post.dev.service.approve.impl;
 
+import com.post.dev.DTO.ApproveListDTO;
 import com.post.dev.service.approve.ApproveService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class ApproveServiceImpl implements ApproveService {
 
+    @Override
+    public List<ApproveListDTO> approveList(int listNum) {
+        return null;
+    }
 
+    @Override
+    public ApproveListDTO approvePost(int approveSeq) {
+        return null;
+    }
 
-
+    @Override
+    public String approve(Map<String, String> approveMessage) {
+        return null;
+    }
 }
